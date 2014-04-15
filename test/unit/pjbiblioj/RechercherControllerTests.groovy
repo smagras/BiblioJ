3bf40c7ec54ffd82ad8867e6afb2e9bf2c5304fd
smagras <magras.steve@gmail.com>
2014-04-15 12:49:37 +0200

    correction bug recherche controller

--- test/unit/pjbiblioj/RechercherControllerTests.groovy
+++ test/unit/pjbiblioj/RechercherControllerTests.groovy
@@ -8,7 +8,7 @@ import org.junit.*
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
-@TestFor(RechercherController)
+@TestFor(Livre)
 class RechercherControllerTests {
 
     void testSomething() {
