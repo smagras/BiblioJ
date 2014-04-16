a99e1223f5383affc0e3ae2ee48c6f5b17f80801
smagras <magras.steve@gmail.com>
2014-04-16 19:50:40 +0200

    création tests pour CSVManager

---
+++ test/unit/com/biblioj/services/CSVManagerTests.groovy
@@ -0,0 +1,15 @@
+package com.biblioj.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+@TestFor(CSVManager)
+class CSVManagerTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
