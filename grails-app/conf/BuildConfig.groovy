4d0304bc92c53282616e976de2c8355412f4d069
smagras <magras.steve@gmail.com>
2014-04-16 16:57:56 +0200

    correction cobertura

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -48,7 +48,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
         runtime ":resources:1.2"
-
+		test ":code-coverage:1.2.7"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
--- grails-app/controllers/pjbiblioj/RechercherController.groovy
+++ grails-app/controllers/pjbiblioj/RechercherController.groovy
@@ -1,6 +1,6 @@
 package pjbiblioj
 
-class RechercheController {
+class RechercherController {
 
 	def scaffold = Livre
 	
