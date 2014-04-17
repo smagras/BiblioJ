fc4816141fbcce3db08b6f085ef3b620039cb243
polotud10 <paul.tudou@gmail.com>
2014-04-17 17:55:55 +0200

    creation des méthodes de recherche de livres

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,13 +1,50 @@
 import pjbiblioj.Auteur
 import pjbiblioj.Livre
+import pjbiblioj.TypeDocument
 
 class BootStrap {
 
     def init = { servletContext ->
+		TypeDocument typeDoc = new TypeDocument(intitule:"Nouveauté")
+		TypeDocument typeDoc2 = new TypeDocument(intitule:"Livre ado")
+		
 		Auteur auteur1 = new Auteur(nom:"Pommaux",prenom:"Yvan").save()
-		Livre livre1 = new Livre(titre:"Ulysse",nombreExemplaires:10,nombreExemplairesDisponibles:3)
+		Auteur auteur2 = new Auteur(nom:"Hugo",prenom:"Victor").save()
+		
+		Livre livre1 = new Livre(titre:"Ulysse",nombreExemplaires:10,nombreExemplairesDisponibles:3,typeDoc:typeDoc)
 		livre1.addToAuteurs(auteur1)
+		livre1.addToAuteurs(auteur2)
 		livre1.save()
+		
+		
+		Livre l2 = new Livre(titre:"Ulysse",nombreExemplaires:2,nombreExemplairesDisponibles:1,typeDoc:typeDoc)
+		l2.addToAuteurs(auteur1)
+		l2.save()
+		
+		
+		Livre l3 = new Livre(titre:"Le petit prince",nombreExemplaires:2,nombreExemplairesDisponibles:1,typeDoc:typeDoc)
+		l3.addToAuteurs(auteur2)
+		l3.save()
+		
+		
+		Livre l4 = new Livre(titre:"Toto",nombreExemplaires:2,nombreExemplairesDisponibles:1,typeDoc:typeDoc)
+		l4.addToAuteurs(auteur1)
+		l4.save()
+		
+		
+		Livre l5 = new Livre(titre:"Titi",nombreExemplaires:2,nombreExemplairesDisponibles:1,typeDoc:typeDoc)
+		l5.addToAuteurs(auteur2)
+		l5.save()
+		
+		
+		Livre l6 = new Livre(titre:"Grosminet",nombreExemplaires:2,nombreExemplairesDisponibles:1,typeDoc:typeDoc)
+		l6.addToAuteurs(auteur1)
+		l6.save()
+		
+		
+		//Livre.rechercherLivreTitre("Ulysse")
+		//Livre.rechercherLivreTypeDoc(typeDoc)
+		Livre.rechercherLivreAuteur("Hugo")
     }
     def destroy = {
     }
--- grails-app/domain/pjbiblioj/Livre.groovy
+++ grails-app/domain/pjbiblioj/Livre.groovy
@@ -19,4 +19,31 @@ class Livre {
 		typeDoc nullable: true
 		auteurs nullable: false
     }
+	
+	
+	
+	def static rechercherLivreTypeDoc(TypeDocument typeDoc) {
+		Livre.findAllByTypeDoc(typeDoc, [max: 5]).each {
+			println it.titre + " " + it.auteurs.nom + " " + it.typeDoc.intitule + " " + it.nombreExemplairesDisponibles
+		}
+	}
+	
+	
+	def static rechercherLivreTitre(String titre) {
+		Livre.findAllByTitre(titre, [max: 5]).each {
+			println it.titre + " " + it.auteurs.nom + " " + it.typeDoc.intitule + " " + it.nombreExemplairesDisponibles
+		}
+	}
+	
+	
+	def static rechercherLivreAuteur(String nom) {
+		/*Auteur auteur = Auteur.findByNom(nom)
+		auteur.livres.each {
+			println it.titre + " " + it.auteurs.nom + " " + it.typeDoc.intitule + " " + it.nombreExemplairesDisponibles
+		}*/
+		
+		Livre.findAll("from Livre as l, Auteur as a where a.nom=:auteur and a in (l.auteurs)", [auteur: nom]).each {
+			println it.titre + " " + it.auteurs.nom + " " + it.typeDoc.intitule + " " + it.nombreExemplairesDisponibles
+		}
+	}
 }
